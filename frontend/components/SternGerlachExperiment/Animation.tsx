1eb2f1423fe68e75dbfa8a715723d3b30c748368
Jakub Rosa <jakub.rosa@itmagination.com>
2024-03-04 20:50:09 +0000

    fix: play with 3d animation

--- frontend/components/SternGerlachExperiment/Animation.tsx
+++ frontend/components/SternGerlachExperiment/Animation.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import { Canvas } from '@react-three/fiber';
-import { CameraControls, CubeCamera, OrbitControls, OrthographicCamera } from '@react-three/drei';
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import { CameraControls, CubeCamera, OrbitControls, OrthographicCamera, PerspectiveCamera } from '@react-three/drei';
+import { useRef, useEffect } from 'react';
 
+// https://discourse.threejs.org/t/extruded-shape-with-hole-missing-inside-mesh/42401
 function Scene() {
   return (
     <>
-      <mesh>
-        <boxGeometry />
+      <mesh position={[0, 10, -70]}>
+        <boxGeometry args={[20, 20, 20]} />
         <meshStandardMaterial />
       </mesh>
-      <mesh position={[10,0,0]}>
+      <mesh position={[10, 1, 0]}>
         <boxGeometry />
         <meshStandardMaterial />
       </mesh>
@@ -22,7 +24,13 @@ export function Animation() {
   return (
     <Canvas>
       <Scene />
-      <gridHelper args={[100, 10]} />
+      <gridHelper args={[1000, 100]} />
+      <PerspectiveCamera
+        makeDefault
+        position={[60, 30, 0]}
+        fov={70}
+        zoom={1}
+      />
       <OrbitControls />
     </Canvas>
   );
